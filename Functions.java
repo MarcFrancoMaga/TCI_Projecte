caa0a66d3fa7c8ff2fd6cd233bcde1a5a664b0ed
David López <lgdavid0194@gmail.com>
2024-09-23 19:48:17 +0200

    fix

--- Functions.java
+++ Functions.java
@@ -1,39 +1,31 @@
-
 import java.io.FileInputStream;
-import java.io.IOException; 
+import java.io.IOException;
 
+public class Functions {
 
-public class Functions()
-{
-    public int [][][] LoadImage(image_file,row,col,components,bytes_sample,signed)
-    {
-        
-        int [][][] image_loaded = new int [row][col][components];
+    public int[][][] LoadImage(String image_file, int row, int col, int components, int bytes_sample, boolean signed) throws IOException {
+        int[][][] image_loaded = new int[row][col][components];
 
         FileInputStream image_input = new FileInputStream(image_file);
-    
-    
-        for(int i = 0; i < components; i++)
-        {
-            for(int j = 0; j < row; j++)
-            {
-                for(int k = 0; k < col; k++)
-                {
+
+        for (int i = 0; i < components; i++) {
+            for (int j = 0; j < row; j++) {
+                for (int k = 0; k < col; k++) {
                     int data = image_input.read();
-                    if(data == -1)
-                    {
-                        System.out.prinln("File read successfully!");
+                    if (data == -1) {
+                        System.out.println("File read successfully!"); 
                         image_input.close();
+                        return image_loaded; 
                     }
-                    image_loaded[i][j][k] = data;
+                    image_loaded[j][k][i] = data; 
                 }
             }
         }
+        image_input.close(); 
+        return image_loaded;
     }
 
-    public float Entropy(image_file)
-    {
-
-}
-
+    public float Entropy(String image_file) {
+        return 0.0f;
+    }
 }
