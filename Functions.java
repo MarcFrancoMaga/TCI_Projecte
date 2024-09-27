c4ec00dc71d3291c8925f1a7a0d9b5a111ccd0af
David Lopez <lgdavid0194@gmail.com>
2024-09-27 12:10:07 +0200

    mas cosas

--- Functions.java
+++ Functions.java
@@ -4,28 +4,53 @@ import java.io.IOException;
 public class Functions {
 
     public int[][][] LoadImage(String image_file, int row, int col, int components, int bytes_sample, boolean signed) throws IOException {
-        int[][][] image_loaded = new int[row][col][components];
-
+        int[][][] image_loaded = new int[components][row][col];
         FileInputStream image_input = new FileInputStream(image_file);
-
+        System.out.println("Reading File"); 
+        //component -> row -> column
         for (int i = 0; i < components; i++) {
             for (int j = 0; j < row; j++) {
                 for (int k = 0; k < col; k++) {
                     int data = image_input.read();
-                    if (data == -1) {
-                        System.out.println("File read successfully!"); 
-                        image_input.close();
-                        return image_loaded; 
-                    }
-                    image_loaded[j][k][i] = data; 
+                    image_loaded[i][j][k] = data; 
                 }
             }
         }
+        System.out.println("File read successfully!"); 
+
         image_input.close(); 
         return image_loaded;
     }
 
-    public float Entropy(String image_file) {
-        return 0.0f;
+    public float Entropy(int image_file[][][]) {
+            int total = 0; // FILAS X COLUMNAS
+            float entropy = 0;
+            int[] frequencies = new int[256];
+            
+        for (int i = 0; i < image_file.length; i++) {
+            for (int j = 0; j < image_file[i].length; j++) {
+                for (int k = 0; k < image_file[i][j].length; k++) {
+                    int value = image_file[i][j][k];
+                    if (value >= 0 && value <= 255) {
+                        frequencies[value]++;
+                    }
+                }
+            }
+            //DIVIDIR CADA CASILLA DEL ARRAY ENTRE EL TOTAL 
+            //LUEGO HACER LA FORMULA DE LA ENTROPIA CON CADA CASILLA
+        for (int i = 0; i < image_file.length; i++) {
+            for (int j = 0; j < image_file[i].length; j++) {
+                for (int k = 0; k < image_file[i][j].length; k++) {
+                    if(image_file_new[i][j][k] == 0){
+                        entropy += 0;
+                    }
+                    else{
+                        entropy += (-image_file_new[i][j][k]) * (Math.log(image_file_new[i][j][k]) / Math.log(2));
+                    }
+                }
+            }
+        }
+        return entropy;
+
     }
 }
--- Main.java
+++ Main.java
@@ -16,16 +16,24 @@ public class Main {
             return;
         }
         int[][][] image;
+        float entropy = 0;
         String path_file = arguments[0];
         int rows = Integer.parseInt(arguments[1]);
         int columns = Integer.parseInt(arguments[2]);
         int components = Integer.parseInt(arguments[3]);
         int bytes_sample = Integer.parseInt(arguments[4]);
         boolean signed = Boolean.parseBoolean(arguments[5]);
-
+        path_file = "../imatges/imatges/n1_GRAY.1_2560_2048_1_0_8_0_0_0.raw";
+        /*
+        rows = 512;
+        columns = 512;
+        components = ;
+        bytes_sample = ;
+        signed = ; */
         Functions functions = new Functions();
         try {
             image = functions.LoadImage(path_file, rows, columns, components, bytes_sample, signed);
+            /* 
             for (int i = 0; i < image.length; i++) {
                 for (int j = 0; j < image[i].length; j++) {
                     for (int k = 0; k < image[i][j].length; k++) {
@@ -34,7 +42,10 @@ public class Main {
                     System.out.println();
                 }
                 System.out.println(); 
-            }
+            }*/
+           entropy = functions.Entropy(image);
+           System.out.println(entropy);
+
         }        
         catch (IOException e) {
             System.out.println("An error occurred while loading the image: " + e.getMessage());
