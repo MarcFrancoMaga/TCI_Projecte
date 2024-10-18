4e1f3cc19fee101983ba5798fc161f880323ba76
David Lopez <lgdavid0194@gmail.com>
2024-10-18 12:17:48 +0200

    cosas

--- Functions.java
+++ Functions.java
@@ -1,4 +1,5 @@
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Functions {
@@ -26,7 +27,7 @@ public class Functions {
         int total = 0;
         float entropy = 0;
         int[] frequencies = new int[256]; 
-    
+        System.out.println("Calculating entropy");
         for (int i = 0; i < image_file.length; i++) {
             for (int j = 0; j < image_file[i].length; j++) {
                 for (int k = 0; k < image_file[i][j].length; k++) {
@@ -55,4 +56,73 @@ public class Functions {
         return entropy;
     }
     
-}
+    public int[][][] Quantization(int image_file[][][], int q_step, int direction) {
+        for (int i = 0; i < image_file.length; i++) {
+            for (int j = 0; j < image_file[i].length; j++) {
+                for (int k = 0; k < image_file[i][j].length; k++) {
+                    if (q_step >= 1) {
+                        if (direction == 0) {
+                            if (image_file[i][j][k] < 0){
+                                image_file[i][j][k] = (int) Math.floor(Math.abs(image_file[i][j][k]) / q_step);
+                                image_file[i][j][k] *= -1; 
+                            }
+                            else {
+                                image_file[i][j][k] = (int) Math.floor(Math.abs(image_file[i][j][k]) / q_step);
+                            }
+                        }
+                        else {
+                            image_file[i][j][k] =  image_file[i][j][k] * q_step;
+                        }
+                       
+                    } else {
+                        System.out.println("q_step less than 1");
+                    }
+                    
+                }
+            }
+        }
+        return image_file;
+    }
+    
+    public void SaveFile(int image_file[][][], int bytes_sample, boolean signed, String path) {
+        try(FileOutputStream image_output = new FileOutputStream(path)) {
+            System.out.println("Saving File");
+            for (int i = 0; i < image_file.length; i++) {
+                for (int j = 0; j < image_file[i].length; j++) {
+                    for (int k = 0; k < image_file[i][j].length; k++) {
+                        int value = image_file[i][j][k];
+
+                        if (bytes_sample == 1) {
+                            image_output.write(value);
+                        }
+                    }
+                }
+            }
+            System.out.println("File saved successfully!");
+        } catch (IOException e) {
+            System.err.println("Error saving file: " + e.getMessage());
+        }
+    }
+
+    public float MSE(int[][][] original_image, int[][][] q_image) {
+        int total = 0;
+        int count = 0;
+        System.out.println("Calculating MSE");
+        for (int i = 0; i < original_image.length; i++) {
+            for (int j = 0; j < original_image[i].length; j++) {
+                for (int k = 0; k < original_image[i][j].length; k++) {
+                    int aux = original_image[i][j][k] - q_image[i][j][k]; 
+                    total += Math.pow(aux, 2);
+                    count++;
+                }
+            }
+        }
+        return (float) total / count;
+    }
+
+    public float PSNR(int[][][] original_image, int[][][] q_image, int mse) {
+        double result = 0;
+        result = 10 * Math.log10((Math.pow(2, 8) - 1) / mse);
+        return (float) result;
+    }
+}   
--- Main.java
+++ Main.java
@@ -16,6 +16,7 @@ public class Main {
             return;
         }
         int[][][] image;
+        int[][][] quantified_image;
         float entropy = 0;
         String path_file = arguments[0];
         int rows = Integer.parseInt(arguments[1]);
@@ -23,13 +24,15 @@ public class Main {
         int components = Integer.parseInt(arguments[3]);
         int bytes_sample = Integer.parseInt(arguments[4]);
         boolean signed = Boolean.parseBoolean(arguments[5]);
-        path_file = "../imatges/n1_GRAY.1_2560_2048_1_0_8_0_0_0.raw";
-        //Enter image data: ../imatges/n1_GRAY.1_2560_2048_1_0_8_0_0_0 2560 2048 1 1 0
+        path_file = "../imatges/imatges/n1_GRAY.1_2560_2048_1_0_8_0_0_0.raw";
+        //Enter image data: ../imatges/n1_GRAY.1_2560_2048_1_0_8_0_0_0 -> 2560 2048 1 1 0
         Functions functions = new Functions();
         try {
             image = functions.LoadImage(path_file, rows, columns, components, bytes_sample, signed);
-            entropy = functions.Entropy(image);
-            System.out.println(entropy);
+            //entropy = functions.Entropy(image);
+            quantified_image = functions.Quantization(image, 125, 0);
+            quantified_image = functions.Quantization(quantified_image, 125, 1);
+            functions.SaveFile(quantified_image, 1, false, "../imatges/prueba.raw");
 
         }        
         catch (IOException e) {
